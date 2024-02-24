2a3d67ae7834c9a636c39143accfd17d194b426a
Soteck <tukav73@gmail.com>
2024-02-24 22:26:42 +0100

    - Fix camera wonkyness, run physics on separated thread

--- scripts/player/player_camera.gd
+++ scripts/player/player_camera.gd
@@ -2,14 +2,16 @@ extends Camera3D
 
 @export var mouse_sensitivity = .2;
 
-@onready var player = $"../..";  
+@onready var player = $"..";
+@onready var alternativeCamera: Camera3D = $"../CanvasLayer/SubViewportContainer/SubViewport/Camera3D";
 
-func _process(delta):
-	position = Vector3(player.position.x, player.position.y + 1.75, player.position.z);
-	rotation = Vector3(rotation.x, player.rotation.y, player.rotation.z);
+
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		player.rotation_degrees.y -= event.relative.x * mouse_sensitivity / 10;
+		#rotation_degrees.y = player.rotation_degrees.y;
 		rotation_degrees.x = clamp(rotation_degrees.x - event.relative.y * mouse_sensitivity / 10, -90, 90);
-
+		if(alternativeCamera):
+			alternativeCamera.rotation = rotation;
+			alternativeCamera.position = position;
