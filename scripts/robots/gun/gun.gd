0aa05cda7fbea3dc8cf661fa3cb018f7a8d70cd1
bocdagla <tomeu1m1a1@gmail.com>
2024-02-25 13:53:37 +0100

    - Base level created

--- scripts/robots/gun/gun.gd
+++ scripts/robots/gun/gun.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @export var rotation_speed: float = 2;
 @export var shoot_cooldown: float = 2;
+@export var damage: float = 5;
 
 @export var target: Node3D;
 
@@ -9,7 +10,7 @@ var fire: bool = false;
 var cooldown: SceneTreeTimer;
 const FLOOR_MASK = 1;  
 
-signal shoot;
+signal shoot(impact: Vector3);
 
 func _process(delta: float):
 	if !target:
@@ -44,9 +45,9 @@ func open_fire():
 		var collision = get_world_3d().direct_space_state.intersect_ray(ray);
 		if collision.is_empty() || !collision.collider.has_method("damage"):
 			return;
-		
-		collision.collider.call("damage");
-		shoot.emit();
+
+		collision.collider.call("damage", damage);
+		shoot.emit(collision.position);
 		
 		cooldown = get_tree().create_timer(shoot_cooldown, false);
 		cooldown.timeout.connect(open_fire);
