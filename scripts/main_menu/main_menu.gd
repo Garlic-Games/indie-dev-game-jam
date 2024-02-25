dba3b3fc24868efde7f91a9f99ce2151834a97b0
Nanocirs <nanocirs@gmail.com>
2024-02-25 16:48:50 +0100

    Polished main menu

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -1,21 +1,40 @@
 extends CanvasLayer;
 
+@onready var main_menu: PanelContainer = %MainContainer;
 @onready var settings_menu: SettingsContainer = %SettingsMenu;
 @onready var credits: Credits = %Credits;
 @onready var loading_panel: LoadingPanel = %LoadingPanel;
 @export_file("*.tscn") var gameplay_scene: String;
 
+
 func _ready():
-	assert(gameplay_scene != "", "A gameplay scene resource must be provided to main menu")
+	assert(gameplay_scene != "", "A gameplay scene resource must be provided to main menu");
+	
+	settings_menu.connect("closed", open);
+	credits.connect("closed", open);
+
+
+func open():
+	main_menu.show();
+	
+	
+func close():
+	main_menu.hide();
+
 
 func start_game() -> void:
 	SceneLoader.load_scene(gameplay_scene);
 
+
 func open_settings() -> void:
+	close();
 	settings_menu.open();
 
+
 func open_credits() -> void:
+	self.close();
 	credits.open();
+
 
 func close_game() -> void:
 	get_tree().quit();
