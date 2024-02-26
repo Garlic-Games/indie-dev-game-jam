7314559077194ae1c711568fcb79d829360230f0
Soteck <tukav73@gmail.com>
2024-02-26 16:01:25 +0100

    - Export configuration

--- scripts/player/mouse_capture_input.gd
+++ scripts/player/mouse_capture_input.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+
 func _input(event):
 	if event is InputEventKey and event.is_action_pressed("pause"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
